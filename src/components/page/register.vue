46fd7e4fa095f0ef8d58ca3896eb7e9dff6d3902
zhengp <1769147730@qq.com>
2020-03-30 22:58:58 +0800

    设置界面

---
+++ src/components/page/register.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="register">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item to:="{path:'/}">登陆</el-breadcrumb-item>
+        <el-breadcrumb-item>注册界面</el-breadcrumb-item>
+      </el-breadcrumb>
+      <h2>注册界面</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"register"
+}
+</script>
+
+<style>
+
+</style>
